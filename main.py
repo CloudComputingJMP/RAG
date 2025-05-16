91ae16f03d788c4efb5d724840d1572eb1242164
shamajn <piotr.rowicki02@gmail.com>
2025-05-16 20:06:41 +0200

    better rest api

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from fastapi import FastAPI
 import uvicorn
 from google.cloud import aiplatform
 from vertexai.language_models import TextEmbeddingModel
+from pydantic import BaseModel
 
 
+class QueryModel(BaseModel):
+    query: str
+    k:int
 
-PROJECT_ID=os.environ.get("PROJECT_ID","aa")
-IDX=os.environ.get("IDX",'x')
-IDX_EP=os.environ.get("IDX_EP",'x')
-IDX_DEP_ID=os.environ.get("IDX_DEP_ID","x")
+PROJECT_ID=os.environ.get("PROJECT_ID","cloudcomputingjmp")
+IDX=os.environ.get("IDX",'projects/865714441621/locations/europe-central2/indexes/7698780417687552000')
+IDX_EP=os.environ.get("IDX_EP",'projects/865714441621/locations/europe-central2/indexEndpoints/7362365043960184832')
+IDX_DEP_ID=os.environ.get("IDX_DEP_ID","ingreedio_vector_index_deployed_idx")
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 aiplatform.init(project=PROJECT_ID, location="europe-central2")
 model = TextEmbeddingModel.from_pretrained("text-multilingual-embedding-002")
@@ -25,13 +29,13 @@ def encode_texts_to_embeddings(sentences: List[str]) -> List[Optional[List[float
     except Exception:
         return [None for _ in range(len(sentences))]
 
-@app.get("/recomendations")
-def recomendations(query:str,k:int)->List[int]:
-    emb=encode_texts_to_embeddings([query])
+@app.post("/recomendations")
+def recomendations(query:QueryModel)->List[int]:
+    emb=encode_texts_to_embeddings([query.query])
     resp=index_endpoint.find_neighbors(
         deployed_index_id=IDX_DEP_ID,
         queries=emb,
-        num_neighbors=k
+        num_neighbors=query.k
     )
     return [int(obj.id) for obj in resp[0]]
 
